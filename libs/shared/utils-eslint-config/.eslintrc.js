9f17ea7335749b4e986ff38d81e51c14680c3ef3
Owen <heewon.dev@gmail.com>
2024-06-01 23:37:59 +0900

    chore: bump package version

--- libs/shared/utils-eslint-config/.eslintrc.js
+++ libs/shared/utils-eslint-config/.eslintrc.js
@@ -1,64 +1,77 @@
 module.exports = {
   root: true,
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:jsonc/recommended-with-json', 'turbo'],
-  plugins: ['prettier', 'import', 'unused-imports'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        trailingComma: 'es5',
-        singleQuote: true,
-        printWidth: 120,
-        arrowParens: 'avoid',
-        endOfLine: 'auto',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx', '*.cjs', '*.mjs'],
+      extends: ['plugin:@typescript-eslint/recommended', 'plugin:jsonc/recommended-with-json', 'turbo'],
+      plugins: ['prettier', 'import', 'unused-imports'],
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
       },
-    ],
-    'import/extensions': ['off'],
-    'import/no-cycle': ['error'],
-    'import/no-extraneous-dependencies': ['off'],
-    'import/no-named-as-default': ['off'],
-    'import/no-relative-packages': ['off'],
-    'import/no-self-import': ['error'],
-    'import/order': [
-      'error',
-      {
-        groups: ['internal', 'external', 'builtin', 'parent', 'sibling'],
-        pathGroups: [
+      rules: {
+        'prettier/prettier': [
+          'error',
           {
-            pattern: '@*/**',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: '@*/**',
-            group: 'external',
-            position: 'after',
+            trailingComma: 'es5',
+            singleQuote: true,
+            printWidth: 120,
+            arrowParens: 'avoid',
+            endOfLine: 'auto',
           },
         ],
-        pathGroupsExcludedImportTypes: [],
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
+        'import/extensions': ['off'],
+        'import/no-cycle': ['error'],
+        'import/no-extraneous-dependencies': ['off'],
+        'import/no-named-as-default': ['off'],
+        'import/no-relative-packages': ['off'],
+        'import/no-self-import': ['error'],
+        'import/order': [
+          'error',
+          {
+            groups: ['internal', 'external', 'builtin', 'parent', 'sibling'],
+            pathGroups: [
+              {
+                pattern: '@*/**',
+                group: 'internal',
+                position: 'before',
+              },
+              {
+                pattern: '@*/**',
+                group: 'external',
+                position: 'after',
+              },
+            ],
+            pathGroupsExcludedImportTypes: [],
+            alphabetize: {
+              order: 'asc',
+              caseInsensitive: true,
+            },
+          },
+        ],
+        'import/prefer-default-export': ['off'],
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          {
+            prefer: 'no-type-imports',
+          },
+        ],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'turbo/no-undeclared-env-vars': ['warn'],
       },
-    ],
-    'import/prefer-default-export': ['off'],
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
-      {
-        prefer: 'no-type-imports',
+    },
+    {
+      files: ['*.json'],
+      parser: 'jsonc-eslint-parser',
+      rules: {
+        'jsonc/sort-keys': ['error'],
       },
-    ],
-    'jsonc/sort-keys': ['error'],
-  },
-  overrides: [
+    },
     {
       files: ['package.json'],
       parser: 'jsonc-eslint-parser',
--- libs/shared/utils-eslint-config/react.js
+++ libs/shared/utils-eslint-config/react.js
@@ -1,7 +1,5 @@
 module.exports = {
   root: true,
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'turbo'],
-  plugins: ['prettier', 'import', 'react'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -9,62 +7,70 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  rules: {
-    'react-hooks/exhaustive-deps': [
-      'warn',
-      {
-        enableDangerousAutofixThisMayCauseInfiniteLoops: true,
-      },
-    ],
-    'prettier/prettier': [
-      'error',
-      {
-        trailingComma: 'es5',
-        singleQuote: true,
-        printWidth: 120,
-        arrowParens: 'avoid',
-        endOfLine: 'auto',
-      },
-    ],
-    'import/extensions': ['off'],
-    'import/no-cycle': ['error'],
-    'import/no-extraneous-dependencies': ['off'],
-    'import/no-named-as-default': ['off'],
-    'import/no-relative-packages': ['off'],
-    'import/no-self-import': ['error'],
-    'import/order': [
-      'error',
-      {
-        groups: ['internal', 'external', 'builtin', 'parent', 'sibling'],
-        pathGroups: [
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx', '*.cjs', '*.mjs'],
+      extends: ['plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'turbo'],
+      plugins: ['prettier', 'import', 'react', 'unused-imports'],
+      rules: {
+        'react-hooks/exhaustive-deps': [
+          'warn',
           {
-            pattern: '@*/**',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: '@*/**',
-            group: 'external',
-            position: 'after',
+            enableDangerousAutofixThisMayCauseInfiniteLoops: true,
           },
         ],
-        pathGroupsExcludedImportTypes: [],
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
+        'prettier/prettier': [
+          'error',
+          {
+            trailingComma: 'es5',
+            singleQuote: true,
+            printWidth: 120,
+            arrowParens: 'avoid',
+            endOfLine: 'auto',
+          },
+        ],
+        'import/extensions': ['off'],
+        'import/no-cycle': ['error'],
+        'import/no-extraneous-dependencies': ['off'],
+        'import/no-named-as-default': ['off'],
+        'import/no-relative-packages': ['off'],
+        'import/no-self-import': ['error'],
+        'import/order': [
+          'error',
+          {
+            groups: ['internal', 'external', 'builtin', 'parent', 'sibling'],
+            pathGroups: [
+              {
+                pattern: '@*/**',
+                group: 'internal',
+                position: 'before',
+              },
+              {
+                pattern: '@*/**',
+                group: 'external',
+                position: 'after',
+              },
+            ],
+            pathGroupsExcludedImportTypes: [],
+            alphabetize: {
+              order: 'asc',
+              caseInsensitive: true,
+            },
+          },
+        ],
+        'import/prefer-default-export': ['off'],
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          {
+            prefer: 'no-type-imports',
+          },
+        ],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'turbo/no-undeclared-env-vars': ['warn'],
       },
-    ],
-    'import/prefer-default-export': ['off'],
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
-      {
-        prefer: 'no-type-imports',
-      },
-    ],
-    'turbo/no-undeclared-env-vars': ['warn'],
-  },
-  overrides: [
+    },
     {
       files: ['package.json'],
       parser: 'jsonc-eslint-parser',
